2ff42a3cba75b9debf51e5b6f4bc97dde4daa56f
ArtemPatazhevskyi <159034846+ArtemPatazhevskyi@users.noreply.github.com>
2025-02-04 16:42:36 +0200

    Lum 76 (#6)
    * add redux
    
    * add Loader on buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import RootRouter from "./router";
+import RegistrationPage from "./pages/RegistrationPage";
 
 function App() {
   return (
     <>
       <RootRouter />
+      {/* <RegistrationPage /> */}
     </>
   );
 }
