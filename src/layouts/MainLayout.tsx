8b62c2c73b501182e97eb9dbd938845f42296d44
Artem Patazhevskyi <artempatazhevskyi@gmail.com>
2025-03-19 21:00:30 +0200

    fix header

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../components/Header/Header.tsx';
+import Header from '../components/header/Header.tsx';
 import { Footer } from '../components/footer/Footer';
 import { Outlet } from 'react-router';
 import { Box } from '@mui/material';
